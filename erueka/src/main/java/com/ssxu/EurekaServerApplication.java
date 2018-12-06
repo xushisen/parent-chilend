bbf4b4846635db517471962b6320ae831f89428f
xushisen <123495896@qq.com>
2018-12-06 13:51:51 +0800

    测试上传是否成功

--- erueka/src/main/java/com/ssxu/EurekaServerApplication.java
+++ erueka/src/main/java/com/ssxu/EurekaServerApplication.java
@@ -16,5 +16,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+        System.out.println("ddddd");
     }
 }
