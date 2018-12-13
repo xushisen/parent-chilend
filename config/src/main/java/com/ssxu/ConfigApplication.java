2e75972084f136f8cf156a0f7e71b9b6508ddd14
xss <xss>
2018-12-13 14:14:57 +0800

    增加配置中心加密解密需要注意的事项

--- config/src/main/java/com/ssxu/ConfigApplication.java
+++ config/src/main/java/com/ssxu/ConfigApplication.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * 类描述：config  http://localhost:8091/encrypt/status
  *                http://localhsot:8091/encrypt -d mysercet
  *                http://localhost:8091/decrypt -d fdasfa2341sdfa134214…
+ *                !!! {cipher} yml前后需要加单引号  propertos不需要
+ *                获取加密解密的内容的时候 bootstarp.yml里面的账号密码去来  不然postman请求加密解密会被拦截
  * 创建人：徐石森
  * 创建时间：2018/12/13  9:14
  *
